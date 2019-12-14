b8891c63a755fb936888f41d45d83adb879a9830
Robert M Ricci <rmricci121@gmail.com>
2019-12-14 11:39:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,8 +24,8 @@ end
 
 def printer(attendees)
   badge_and_room_assignments = []
-  batch_badge_creator.each do |attendees|
-    badge_and_room_assignments << attendees
+  batch_badge_creator.each do |attendee|
+    badge_and_room_assignments << attendee
   end
   
 end
